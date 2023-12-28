82b10f88b3c592e049a4055dc976e501e42f950d
Mehmed Zahid Binici <mehmet2002.b@gmail.com>
2023-12-29 00:04:41 +0300

    project update.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 var app = builder.Build();
 
+// ...
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
